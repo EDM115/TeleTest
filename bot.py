57aa77061409b7cf0403c06cddd236a7053a4659
EDM 1.15 <82015596+EDM115@users.noreply.github.com>
2022-02-08 19:04:49 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+import asyncio
+import config
+from pyrogram import Client, filters, StopPropagation
+from ​pyrogram​.​types import ReplyKeyboardMarkup​, ​InlineKeyboardMarkup​, ​InlineKeyboardButton
+
+BOT_TOKEN​ ​=​ ​config​.​BOT_TOKEN 
+​APP_ID​ ​=​ ​config​.​APP_ID 
+API_HASH​ ​=​ ​config​.​API_HASH
+
+#async def main():
+#    async with Client("my_account", api_id, api_hash) as app:
+#        await app.send_message("me", "Greetings from **Pyrogram**!")
+#asyncio.run(main())
+
+bot = Client(
+	"TeleTest"​,
+	​bot_token​=​BOT_TOKEN​,
+	​api_id​=​APP_ID​,
+	​api_hash​=​API_HASH
+	).​run​()
+
+@​bot​.​on_message​(​filters​.​command​([​"start"​])) 
+async​ ​def​ ​start​(​client​, ​message​):
+	joinButton​ ​=​ ​InlineKeyboardMarkup​([
+		[​InlineKeyboardButton​(​"Channel"​, ​url​=​"https://t.me/EDM115bots"​)],
+		[​InlineKeyboardButton​(
+			"Report Bugs 😊"​, ​url​=​"https://t.me/EDM115"​)]
+	])
+	welcomed​ ​=​ ​f"Hey <b>​{​message​.​from_user​.​first_name​}​</b>​\n​/help for More info"
+	await​ ​message​.​reply_text​(​welcomed​, ​reply_markup​=joinButton)
+	raise​ ​StopPropagation
